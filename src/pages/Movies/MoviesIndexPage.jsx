618a11827e62d98078a936d77011d2d8e8041927
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-13 16:44:57 +0100

    show and index functions from frontend

--- src/pages/Movies/MoviesIndexPage.jsx
+++ src/pages/Movies/MoviesIndexPage.jsx
@@ -1,8 +1,9 @@
 // IMPORT REACT HOOKS
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function MoviesIndexPage() {
-  /* const serverUrl = import.meta.env.VITE_SERVER_URL;
+  const serverUrl = import.meta.env.VITE_SERVER_URL + "/api/movies";
 
   const [movies, setMovies] = useState([]);
 
@@ -11,9 +12,8 @@ export default function MoviesIndexPage() {
       .then((res) => res.json())
       .then((data) => {
         setMovies(data);
-        console.log(data);
       });
-  } */
+  }
 
   useEffect(fetchMovies, []);
 
@@ -21,6 +21,14 @@ export default function MoviesIndexPage() {
     <>
       <div className="container">
         <h1 className="pt-3">Movies</h1>
+
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={"/movies/" + movie.id}>{movie.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
--- src/pages/Movies/MoviesShowPage.jsx
+++ src/pages/Movies/MoviesShowPage.jsx
@@ -1,15 +1,27 @@
 // IMPORT REACT HOOKS
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function MoviesShowPage() {
   const { id } = useParams();
+  const serverUrl = import.meta.env.VITE_SERVER_URL + "/api/movies/" + id;
 
-  useEffect(() => {}, []);
+  const [movie, setMovie] = useState([]);
+
+  // FETCH FUNCTION FOR SINGLE MOVIE
+  function fetchMovie() {
+    fetch(serverUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data);
+      });
+  }
+  useEffect(fetchMovie, []);
 
   return (
     <>
       <div className="container">
-        <h1 className="pt-3">Movie {id}</h1>
+        <h1 className="pt-3">{movie.title}</h1>
       </div>
     </>
   );
