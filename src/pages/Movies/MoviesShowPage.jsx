3e8097506611a95c0e518ee476023a43a3bbd74a
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-13 18:06:50 +0100

    reviews

--- src/pages/Movies/MoviesShowPage.jsx
+++ src/pages/Movies/MoviesShowPage.jsx
@@ -21,10 +21,35 @@ export default function MoviesShowPage() {
   }
   useEffect(fetchMovie, []);
 
+  console.log(movie.reviews);
+
   return (
     <>
       <div className="container">
-        <Card movie={movie} />
+        <section className="card-movie">
+          <Card movie={movie} />
+        </section>
+
+        <section className="review">
+          <div className="row">
+            {movie.reviews &&
+              movie.reviews.map((review, index) => (
+                <div key={index} className="col">
+                  <div className="card">
+                    <div className="card-header">
+                      <div className="row">
+                        <div className="col">{review.reviews_author_name}</div>
+                        <div className="col text-end vote">{review.vote}</div>
+                      </div>
+                    </div>
+                    <div className="card-body">
+                      <p className="card-text">{review.text}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </section>
       </div>
     </>
   );
